8514ce66b4e37a08df5355e49d66cafedb567125
Ana Riquele <riquelegeraldo@gmail.com>
2023-05-20 10:50:57 -0300

    Nova Versão

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 def fibonacci(num):
     if num < 0:
         raise ValueError("n tem que ser maior do que zero")
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    if num == 2:
+        return 1
     primeiro_termo = 0
     segundo_termo = 1
-    contador = 3
-    print(f"{primeiro_termo} - {segundo_termo}", end=" ")
-    while contador <= num:
+    contador = 3    
+    while contador <= num+1:
         terceiro_termo = primeiro_termo + segundo_termo
-        print(f" - {terceiro_termo}", end=" ")
         primeiro_termo = segundo_termo
         segundo_termo = terceiro_termo
         contador += 1
-
-num = int(input("Digite um número: "))
-fibonacci(num)
+    return terceiro_termo
+if __name__=="__main__":
+    num = int(input("Digite um número: "))
+    print(fibonacci(num))
